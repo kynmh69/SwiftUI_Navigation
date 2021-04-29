5bbe3476605437c37984ab0356dfb516413a1567
kynmh69 <hiroki.kayanuma@gmail.com>
2021-04-29 14:51:58 +0900

    Initial Commit

---
+++ SwiftUI_Navigation/SwiftUI_Navigation/SwiftUI_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationApp.swift
+//  SwiftUI_Navigation
+//
+//  Created by Hiroki Kayanuma on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
